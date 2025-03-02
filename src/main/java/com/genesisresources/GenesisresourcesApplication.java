926ada2d78843460a4a6cdd881a874130d55f006
Sabina Penglová <ssaabina@seznam.cz>
2025-03-02 20:57:49 +0100

    Hlavní třída aplikace (@SpringBootApplication). Spouští celý Spring Boot projekt.

--- src/main/java/com/genesisresources/GenesisresourcesApplication.java
+++ src/main/java/com/genesisresources/GenesisresourcesApplication.java
@@ -1,6 +1,5 @@
 package com.genesisresources;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +9,4 @@ public class GenesisresourcesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GenesisresourcesApplication.class, args);
 	}
-
 }
